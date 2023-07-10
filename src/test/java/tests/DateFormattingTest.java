37cd61ee7cb1c4ac8c0763b606508fbe453b031a
Bruno Borges <brborges@microsoft.com>
2023-07-10 11:45:50 -0700

    Use specfic date

--- src/test/java/tests/DateFormattingTest.java
+++ src/test/java/tests/DateFormattingTest.java
@@ -1,23 +1,27 @@
 package tests;
 
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 
 public class DateFormattingTest {
+
+    private static final String DATE = "2023-07-10T00:00:00.000Z";
+
+    private LocalDateTime getLocalDateTime() {
+        return LocalDateTime.parse(DATE, DateTimeFormatter.ISO_DATE_TIME);
+    }
     
     @Test
     public void test() {
         // Get current date, then format to day of week, and assert output
 
         Locale locale = new Locale("en_US");
-        ZonedDateTime now = ZonedDateTime.now();
-        String expected = "Today is " + now.getDayOfWeek().toString();
-        System.out.println(expected);
-
-        String formatted = DateTimeFormatter.ofPattern("EEEE").withLocale(locale).format(now);
+        Temporal certainDate = getLocalDateTime();
+        String formatted = DateTimeFormatter.ofPattern("EEEE").withLocale(locale).format(certainDate);
         System.out.println(formatted);
     }
 
